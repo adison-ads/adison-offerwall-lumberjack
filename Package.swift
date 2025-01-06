e0847e360720e977aac1ffe0f0845aed330093e3
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2025-01-06 06:35:42 +0000

    0.9.6 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "0.9.4"
-let checksum = "3519620ed66f8a182c65d402373bb388e18c0a472a433414a9f160b64c728968"
+let version = "0.9.6"
+let checksum = "1ba293cec112abf032c1dea9a72fee0c3edd2e5e8d74bcd33691348b60408852"
 
 let package = Package(
     name: "AdisonOfferwallLumberjack",
